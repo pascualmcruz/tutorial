fbf2a356e805027ae1c812d7a550e36d2e5ea372
pascualmcruz <pascualmc62@gmail.com>
2021-04-23 17:08:47 -0500

    first commit

---
+++ modules.js
@@ -0,0 +1,10 @@
+//Modules
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./alt-flavor')
+require('./mind-grenade')
+console.log(data)
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
